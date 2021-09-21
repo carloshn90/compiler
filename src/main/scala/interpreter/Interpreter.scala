5b2bfc32ec8d1aa529f175d4d4aa6c04663bd72f
carloshn90 <carlohn90@gmail.com>
2021-09-21 16:34:25 +0100

    fix bug returning error in the interpreter

--- src/main/scala/interpreter/Interpreter.scala
+++ src/main/scala/interpreter/Interpreter.scala
@@ -2,18 +2,14 @@ package org.compiler.example
 package interpreter
 
 import error.ErrorCompiler
-import interpreter.InterResult.{InterResult, unit}
+import interpreter.InterResult.{InterResult, unit, InterResultMonad}
 import interpreter.stmt.InterStmt.execute
 import parser.stmt.Stmt
-
-import cats.implicits.catsSyntaxApply
 
 class Interpreter {
 
   def interpreter(stmtList: List[Stmt], carryResult: Either[ErrorCompiler, List[String]]): InterResult[List[String]] = env => stmtList match {
     case List() =>  unit(carryResult)(env)
-    case h::t   =>
-      val (result, e) = execute(h)(env)
-      interpreter(t, result.map2(carryResult)((r, carry) => carry:::r.printList))(e)
+    case h::t   => execute(h).flatMap(r => interpreter(t, carryResult.map(carry => carry ::: r.printList)))(env)
   }
 }
